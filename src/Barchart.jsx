16dfa391c1bd48d622247af0bc9dc153a1f96af4
Sarthakparmar-04 <singhsarthak691@gmail.com>
2023-08-05 01:07:00 +0530

    update barchart

--- src/Barchart.jsx
+++ src/Barchart.jsx
@@ -12,75 +12,88 @@ const BarChart = () => {
   const svgRef = useRef(null);
 
   useEffect(() => {
-    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const margin = { top: 20, right: 20, bottom: 50, left: 40 }; // Adjusted bottom margin to show the x-axis labels
+    const width = svgRef.current.parentElement.clientWidth;
+    const height = 300;
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
-    const updateChart = () => {
-      const width = svgRef.current.parentElement.clientWidth;
-      const height = 300;
-      const innerWidth = width - margin.left - margin.right;
-      const innerHeight = height - margin.top - margin.bottom;
+    // Update scales
+    const xScale = d3
+      .scaleBand()
+      .domain(data.map((d) => d.label))
+      .range([0, innerWidth])
+      .padding(0.1);
 
-      // Update scales
-      const xScale = d3
-        .scaleBand()
-        .domain(data.map((d) => d.label))
-        .range([0, innerWidth])
-        .padding(0.1);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.value)])
+      .nice()
+      .range([innerHeight, 0]);
 
-      const yScale = d3
-        .scaleLinear()
-        .domain([0, d3.max(data, (d) => d.value)])
-        .nice()
-        .range([innerHeight, 0]);
+    // Select the SVG container
+    const svg = d3.select(svgRef.current);
 
-      // Select the SVG container
-      const svg = d3.select(svgRef.current);
+    // Update the bars
+    const bars = svg.selectAll("rect").data(data);
 
-      // Update the bars
-      const bars = svg.selectAll("rect").data(data);
+    bars
+      .enter()
+      .append("rect")
+      .attr("x", (d) => xScale(d.label))
+      .attr("y", innerHeight) // For initial animation, set the bars to start from the bottom of the chart
+      .attr("width", xScale.bandwidth())
+      .attr("height", 0) // For initial animation, set the height of the bars to 0
+      .attr("fill", "steelblue")
+      .merge(bars) // Merge enter and existing bars
+      .transition() // Add animation
+      .duration(1000) // Set the animation duration (in milliseconds)
+      .attr("x", (d) => xScale(d.label))
+      .attr("y", (d) => yScale(d.value))
+      .attr("width", xScale.bandwidth())
+      .attr("height", (d) => innerHeight - yScale(d.value));
 
-      bars
-        .enter()
-        .append("rect")
-        .attr("x", (d) => xScale(d.label))
-        .attr("y", innerHeight) // For initial animation, set the bars to start from the bottom of the chart
-        .attr("width", xScale.bandwidth())
-        .attr("height", 0) // For initial animation, set the height of the bars to 0
-        .attr("fill", "steelblue")
-        .merge(bars) // Merge enter and existing bars
-        .transition() // Add animation
-        .duration(1000) // Set the animation duration (in milliseconds)
-        .attr("x", (d) => xScale(d.label))
-        .attr("y", (d) => yScale(d.value))
-        .attr("width", xScale.bandwidth())
-        .attr("height", (d) => innerHeight - yScale(d.value));
+    bars
+      .exit() // Remove any bars that no longer have data
+      .remove();
 
-      bars
-        .exit() // Remove any bars that no longer have data
-        .remove();
+    // Update the x-axis
+    svg
+      .select(".x-axis")
+      .attr("transform", `translate(0, ${innerHeight})`)
+      .call(d3.axisBottom(xScale));
 
-      // Update the x-axis
-      svg
-        .select(".x-axis")
-        .attr("transform", `translate(0, ${innerHeight})`)
-        .call(d3.axisBottom(xScale));
+    // Update the y-axis
+    svg.select(".y-axis").call(d3.axisLeft(yScale));
 
-      // Update the y-axis
-      svg.select(".y-axis").call(d3.axisLeft(yScale));
-    };
+    // Update the chart title
+    svg
+      .select(".chart-title")
+      .attr("x", width / 2)
+      .attr("y", 20)
+      .attr("text-anchor", "middle")
+      .text("Simple Bar Chart");
 
-    // Initial chart render
-    updateChart();
+    // Update the x-axis label
+    svg
+      .select(".x-axis-label")
+      .attr("x", width / 2)
+      .attr("y", height - 5)
+      .attr("text-anchor", "middle")
+      .text("Categories");
 
-    // Add event listener for window resize
-    window.addEventListener("resize", updateChart);
-
-    // Cleanup the event listener on unmount
-    return () => window.removeEventListener("resize", updateChart);
+    // Update the y-axis label
+    svg
+      .select(".y-axis-label")
+      .attr("x", -height / 2)
+      .attr("y", margin.left / 2)
+      .attr("transform", "rotate(-90)")
+      .attr("text-anchor", "middle")
+      .text("Values");
   }, [data]);
 
   return (
-    <div>
+    <div className="mx-6">
       <svg ref={svgRef} width="100%" height={300}>
         <g className="x-axis" />
         <g className="y-axis" />
